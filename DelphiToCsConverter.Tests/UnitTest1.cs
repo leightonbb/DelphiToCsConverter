6e3a7e54a4d00c91874bf794d678e2895026d0bd
leigh <leigh@LDESKTOP-201608>
2016-09-09 15:15:47 +1200

    Added test for different unit name and class name.

--- DelphiToCsConverter.Tests/UnitTest1.cs
+++ DelphiToCsConverter.Tests/UnitTest1.cs
@@ -10,29 +10,31 @@ namespace DelphiToCsConverter.Tests
         [TestMethod]
         public void Call_Convert_WhenSingleMethod()
         {
-            var delphiCode = @"
+            TestConversion(@"
+unit Unit1;
+
+interface
+
+implementation
+
 procedure TForm1.Button1Click(Sender: TObject);
     begin
     end; 
-";
+",
 
-            var expectedCsCode = @"
+@"
 class TForm1
 {
     void Button1Click(Sender: TObject)
     {
     }
 };
-";
-            var target = new DelphiToCsConverter.Converter();
-            var actual = target.Convert(delphiCode);
-
-            Assert.AreEqual(expectedCsCode, actual);
+");
         }
         [TestMethod]
-        public void Call_Convert_WhenEmptyClass()
+        public void Call_Convert_WhenEmptyClassWithUnit1Name()
         {
-            var delphiCode = @"
+            TestConversion(@"
 unit Unit1;
 
 interface
@@ -41,12 +43,21 @@ type Name = class;
 
 implementation
 
-end.";
+end.",
 
-            var expectedCsCode = @"
+@"
+namespace Unit1
+{
+    
+    
     public class Name
     {
-    }";
+    }
+}"
+);
+        }
+        private static void TestConversion(string delphiCode, string expectedCsCode)
+        {
             var target = new DelphiToCsConverter.Converter();
             var actual = target.Convert(delphiCode);
 
@@ -54,6 +65,31 @@ end.";
 
             actual.Should().Contain(expectedCsCode);
         }
-        
+
+        [TestMethod]
+        public void Call_Convert_WhenEmptyClassWithUnit2Name2()
+        {
+            TestConversion(@"
+unit Unit2;
+
+interface
+
+type Name2 = class;
+
+implementation
+
+end.",
+
+@"
+namespace Unit2
+{
+    
+    
+    public class Name2
+    {
+    }
+}"
+);
+        }
     }
 }
